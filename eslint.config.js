dcbff86b66918a77e536f8111fe1d60c32ffbeaf
Osmanys Fuentes L <ossmanys@gmail.com>
2026-03-12 12:08:40 +0400

    Added eslint rule to remove unnecessary curly braces

--- eslint.config.js
+++ eslint.config.js
@@ -3,7 +3,7 @@ import globals from "globals";
 import reactHooks from "eslint-plugin-react-hooks";
 import reactRefresh from "eslint-plugin-react-refresh";
 import tseslint from "typescript-eslint";
-import { defineConfig, globalIgnores } from "eslint/config";
+import {defineConfig, globalIgnores} from "eslint/config";
 import eslintConfigPrettier from "eslint-config-prettier";
 
 export default defineConfig([
@@ -21,5 +21,8 @@ export default defineConfig([
       ecmaVersion: 2020,
       globals: globals.browser,
     },
+    rules: {
+      "react/jsx-curly-brace-presence": ["error", { "props": "never", "children": "never" }]
+    }
   },
 ]);
