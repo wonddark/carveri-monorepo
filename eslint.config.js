76ad62dabe22ae80d9c7e2c063e2348a8f7acc32
Osmanys Fuentes L <ossmanys@gmail.com>
2026-03-12 13:29:03 +0400

    Added some linter rules for code quality

--- eslint.config.js
+++ eslint.config.js
@@ -3,8 +3,10 @@ import globals from "globals";
 import reactHooks from "eslint-plugin-react-hooks";
 import reactRefresh from "eslint-plugin-react-refresh";
 import tseslint from "typescript-eslint";
-import {defineConfig, globalIgnores} from "eslint/config";
+import { defineConfig, globalIgnores } from "eslint/config";
 import eslintConfigPrettier from "eslint-config-prettier";
+import eslintReact from "@eslint-react/eslint-plugin";
+import stylistic from "@stylistic/eslint-plugin";
 
 export default defineConfig([
   globalIgnores(["dist"]),
@@ -15,14 +17,19 @@ export default defineConfig([
       tseslint.configs.recommended,
       reactHooks.configs.flat.recommended,
       reactRefresh.configs.vite,
+      eslintReact.configs["recommended-typescript"],
       eslintConfigPrettier,
     ],
+    plugins: { "@stylistic": stylistic },
     languageOptions: {
       ecmaVersion: 2020,
       globals: globals.browser,
     },
     rules: {
-      "react/jsx-curly-brace-presence": ["error", { "props": "never", "children": "never" }]
-    }
+      "@stylistic/jsx-curly-brace-presence": [
+        "warn",
+        { props: "never", children: "never", propElementValues: "always" },
+      ],
+    },
   },
 ]);
