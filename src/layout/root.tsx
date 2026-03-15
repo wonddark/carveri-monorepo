b6d36626cfe3b424d81b3f2748e7cedc5358722a
Osmanys Fuentes L <ossmanys@gmail.com>
2026-03-15 15:58:50 +0400

    feat: add login and Reporte gratis links to root nav

--- src/layout/root.tsx
+++ src/layout/root.tsx
@@ -5,18 +5,29 @@ import {
   NavigationMenuLink,
   NavigationMenuList,
 } from "@/components/ui/navigation-menu.tsx";
+import { Button } from "@/components/ui/button.tsx";
 import { Toaster } from "@/components/ui/sonner.tsx";
 
 function RootLayout() {
   return (
     <div className="flex min-h-screen flex-col">
       <div className="mx-auto w-full max-w-380 px-4 lg:px-16">
-        <NavigationMenu>
+        <NavigationMenu className="ml-auto">
           <NavigationMenuList>
+            <NavigationMenuItem>
+              <NavigationMenuLink asChild>
+                <NavLink to="/login">Iniciar sesión</NavLink>
+              </NavigationMenuLink>
+            </NavigationMenuItem>
             <NavigationMenuItem>
               <NavigationMenuLink asChild>
                 <NavLink to="/register">Registro</NavLink>
               </NavigationMenuLink>
+            </NavigationMenuItem>
+            <NavigationMenuItem>
+              <Button asChild>
+                <NavLink to="/">Reporte gratis</NavLink>
+              </Button>
             </NavigationMenuItem>
           </NavigationMenuList>
         </NavigationMenu>
