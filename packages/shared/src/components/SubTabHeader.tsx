4e1112ac9676aed1eb778ad6b365b1e783082d3e
Osmanys Fuentes L <ossmanys@gmail.com>
2026-04-11 14:33:03 +0400

    style(shared): adjust SubTabHeader typography for improved readability
    - Increase title size on large screens (`text-xl` → `text-2xl`)
    - Update subtitle size for better scaling on large screens (`text-xs` → `text-sm`)

--- packages/shared/src/components/SubTabHeader.tsx
+++ packages/shared/src/components/SubTabHeader.tsx
@@ -7,8 +7,10 @@ function SubTabHeader(props: Readonly<Props>) {
   const { title, subtitle } = props;
   return (
     <div>
-      <h2 className="text-lg font-semibold lg:text-xl">{title}</h2>
-      <p className="text-muted-foreground -mt-0.5 mb-5 text-xs">{subtitle}</p>
+      <h2 className="text-lg font-semibold lg:text-2xl">{title}</h2>
+      <p className="text-muted-foreground -mt-0.5 mb-5 text-xs lg:text-sm">
+        {subtitle}
+      </p>
     </div>
   );
 }
