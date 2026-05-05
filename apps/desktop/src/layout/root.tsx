989a5956acaf5a502d48ee5bb92c623584d261af
Osmanys F. Lomba <ossmanys@gmail.com>
2026-05-05 13:27:04 +0400

    feat(desktop): initialize and store client fingerprint in RootLayout
    - Add `useEffect` to generate and cache unique client fingerprint using `getFingerprint`.
    - Save fingerprint to `localStorage` if not already present.

--- apps/desktop/src/layout/root.tsx
+++ apps/desktop/src/layout/root.tsx
@@ -1,8 +1,21 @@
 import { Outlet } from "react-router";
 import { Toaster } from "@carveri/shared/components/ui/sonner.tsx";
 import { TooltipProvider } from "@carveri/shared/components/ui/tooltip.tsx";
+import { useEffect } from "react";
+import { getFingerprint } from "@carveri/shared/lib/utils";
 
 export default function RootLayout() {
+  useEffect(() => {
+    let fingerprint: string | null;
+    fingerprint = localStorage.getItem("carveri-fp");
+    if (!fingerprint) {
+      (async () => {
+        fingerprint = await getFingerprint();
+        localStorage.setItem("carveri-fp", fingerprint);
+      })();
+    }
+  }, []);
+
   return (
     <div className="min-h-screen">
       <TooltipProvider>
