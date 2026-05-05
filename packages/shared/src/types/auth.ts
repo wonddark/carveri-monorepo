98e4a4bfc437e50d2f8499b6ada7fa7b61a24dc6
Osmanys F. Lomba <ossmanys@gmail.com>
2026-05-05 13:16:38 +0400

    feat(shared): add auth-related types for Register and SendOTP APIs

---
+++ packages/shared/src/types/auth.ts
@@ -0,0 +1,55 @@
+export type RegisterPayload = {
+  name: string;
+  lastName: string;
+  middleName?: string;
+  phoneNumber?: string;
+  email?: string;
+};
+
+export type RegisterResponse = {
+  succeeded: boolean;
+  data: {
+    id: string;
+    phoneNumber: string;
+    email: string;
+    name: string;
+    lastName: string;
+    middleName: string;
+    active: boolean;
+    isVerified: boolean;
+    lastLoginAt: string;
+    loginCount: number;
+  };
+  message: string;
+  error: {
+    code: string;
+    messages: string[];
+  };
+  statusCode: number;
+};
+
+export type SendOTPPayload = {
+  phoneNumber?: string;
+  email?: string;
+  language: string;
+};
+
+export type SendOTPResponse = {
+  succeeded: boolean;
+  data: {
+    isSuccess: boolean;
+    message: string;
+    clientId: string;
+    isVerified: boolean;
+    token: string;
+    tokenExpiresAt: string;
+    rawRefreshToken: string;
+    refreshTokenExpiresAt: string;
+  };
+  message: string;
+  error: {
+    code: string;
+    messages: string[];
+  };
+  statusCode: number;
+};
